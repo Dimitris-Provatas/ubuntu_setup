c06db4578b8b421cad4113f069d292c11128bc40
Dimitris Provatas <james.provatas@gmail.com>
2022-02-07 17:53:52 +0200

    Removed an unneeded sudo

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ apt install software-properties-common
 add-apt-repository ppa:ondrej/php
 apt update
 apt-get install php8.0-cli php8.0-common php8.0-mysql php8.0-curl php8.0-xml php8.0-mbstring
-curl -sS https://getcomposer.org/installer -o composer-setup.php sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 usermod -a -G docker $USER
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo “\nalias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'” >> /home/$USER/.bashrc
